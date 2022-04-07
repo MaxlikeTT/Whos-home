d2c3696a1316fcc26682a0c2c878d69ff149f666
Max Taylor <max.taylor12@yahoo.com>
2022-04-07 15:39:50 -0700

    added kka and italics and padding on test canvas

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 
+def font_config(widget, fontslant, event):
+    widget.configure(font=fontslant)
+
 #create window
 window = tk.Tk()    
 window.configure(bg='pink')
@@ -10,11 +13,13 @@ window.title("Who's home?")
 
 ########
 # Top Label
-label = tk.Label(window, text="Current occupants", font=("Arial", 70), bg='pink', fg='white')
+label = tk.Label(window, text="Current occupants", font=("swiss", 70), bg='pink', fg='white',relief = "sunken")
+label2 = tk.Label(window, text="Kappa Kappa Alpha KKA", font=("swiss", 20, "italic"), bg='pink', fg='white')
 label.pack()
+label2.pack(pady=10, padx=100)
 
-f = frame.tkk
-f2 = frame.tkk
+f = tkk.frame
+f2 = tkk.frame
 f.pack
 f2.pack
 
@@ -23,10 +28,13 @@ f2.pack
 
 #Canvas boxes for names
 #create a canvas to put the pic on
-Max=Canvas(f, height=100, width=100)
-picture_file = PhotoImage(file = 'C:\Desktop\pic.gif') 
+Max=Canvas(tk, height=100, width=100, bg="white")
+Max.pack(pady=20, ipady =10, ipadx = 10)
+picture_file = PhotoImage(file = "C:\pics\gsdgif.gif") 
 Max.create_image(100, 0, anchor=NE, image=picture_file)
-#max.pack(side="left", fill="both", expand=True)
+Max.pack(side="left", fill="both", expand=True)
+
+
 #Max.place(x=262, y=357)
 
 
